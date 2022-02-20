df6057b6bfa151b038b3e2bdf758cebc46124066
Mario <mariomass23@gmail.com>
2022-02-20 15:06:48 -0300

    primera entrega 21/2

--- script.js
+++ script.js
@@ -230,104 +230,229 @@
 //###################################################################################
 
 //////////////////////// DESAFIO COMPLEMENTARIO 16/02 ///////////////////////////////
-let entrada;
-let ingresarNumero;
-let ingresarNumeroSeg;
-let ingresarOp;
-let resultado;
-let cuenta = 0;
-let cuentaSuma = 0;
-let cuentaResta = 0;
-let cuentaDiv = 0;
-let cuentaMulti = 0;
-const arrSumaTotal = [];
-const arrRestaTotal = [];
-const arrDivTotal = [];
-const arrMultiTotal = [];
+// let entrada;
+// let ingresarNumero;
+// let ingresarNumeroSeg;
+// let ingresarOp;
+// let resultado;
+// let cuenta = 0;
+// let cuentaSuma = 0;
+// let cuentaResta = 0;
+// let cuentaDiv = 0;
+// let cuentaMulti = 0;
+// const arrSumaTotal = [];
+// const arrRestaTotal = [];
+// const arrDivTotal = [];
+// const arrMultiTotal = [];
 
-const preguntaNumero = () => {
-    if (cuenta < 1) {
-       ingresarNumero = parseInt(prompt("¿Hacemos unas cuentas? Ingresá un número"));  
-    } else {
-        ingresarNumero = parseInt(prompt("Ingresá un número"));  
-    }
-        ingresarNumeroSeg = parseInt(prompt("Ingresá otro número")); 
-}      
+// const preguntaNumero = () => {
+//     if (cuenta < 1) {
+//        ingresarNumero = parseInt(prompt("¿Hacemos unas cuentas? Ingresá un número"));  
+//     } else {
+//         ingresarNumero = parseInt(prompt("Ingresá un número"));  
+//     }
+//         ingresarNumeroSeg = parseInt(prompt("Ingresá otro número")); 
+// }      
       
-const preguntaOperacion = () => {
-    while (isNaN(ingresarNumero) || isNaN(ingresarNumeroSeg)) {
-        alert("Ingresá un número válido");
-        preguntaNumero();
-    }
-    do {
-        ingresarOp = prompt("Ingresá una operación (+, -, / o *)");
-        switch (ingresarOp) {
-            case "+":
-                resultado = ingresarNumero + ingresarNumeroSeg;
-                cuenta++;
-                cuentaSuma++;
-                const arrSuma = [ingresarNumero, ingresarOp, ingresarNumeroSeg, "=", resultado];
-                arrSumaTotal.push(arrSuma.join(" "));
-                 break;
-             case "-":
-                resultado = ingresarNumero - ingresarNumeroSeg;
-                cuenta++;
-                cuentaResta++;
-                const arrResta = [ingresarNumero, ingresarOp, ingresarNumeroSeg, "=", resultado];
-                arrRestaTotal.push(arrResta.join(" "));
-                 break;
-            case "/":
-                resultado = ingresarNumero / ingresarNumeroSeg;
-                cuenta++;
-                cuentaDiv++;
-                const arrDiv = [ingresarNumero, ingresarOp, ingresarNumeroSeg, "=", resultado];
-                arrDivTotal.push(arrDiv.join(" "));
-                 break;
-             case "*":
-                resultado = ingresarNumero * ingresarNumeroSeg;
-                cuenta++;
-                cuentaMulti++;
-                const arrMulti = [ingresarNumero, ingresarOp, ingresarNumeroSeg, "=", resultado];
-                arrMultiTotal.push(arrMulti.join(" "));
-                 break;
-            default:
-                alert("Ingresá una operación valida")
-                break;
-        }
-    } while (ingresarOp !== "+" && ingresarOp !== "-" && ingresarOp !== "/" && ingresarOp !== "*");
+// const preguntaOperacion = () => {
+//     while (isNaN(ingresarNumero) || isNaN(ingresarNumeroSeg)) {
+//         alert("Ingresá un número válido");
+//         preguntaNumero();
+//     }
+//     do {
+//         ingresarOp = prompt("Ingresá una operación (+, -, / o *)");
+//         switch (ingresarOp) {
+//             case "+":
+//                 resultado = ingresarNumero + ingresarNumeroSeg;
+//                 cuenta++;
+//                 cuentaSuma++;
+//                 const arrSuma = [ingresarNumero, ingresarOp, ingresarNumeroSeg, "=", resultado];
+//                 arrSumaTotal.push(arrSuma.join(" "));
+//                  break;
+//              case "-":
+//                 resultado = ingresarNumero - ingresarNumeroSeg;
+//                 cuenta++;
+//                 cuentaResta++;
+//                 const arrResta = [ingresarNumero, ingresarOp, ingresarNumeroSeg, "=", resultado];
+//                 arrRestaTotal.push(arrResta.join(" "));
+//                  break;
+//             case "/":
+//                 resultado = ingresarNumero / ingresarNumeroSeg;
+//                 cuenta++;
+//                 cuentaDiv++;
+//                 const arrDiv = [ingresarNumero, ingresarOp, ingresarNumeroSeg, "=", resultado];
+//                 arrDivTotal.push(arrDiv.join(" "));
+//                  break;
+//              case "*":
+//                 resultado = ingresarNumero * ingresarNumeroSeg;
+//                 cuenta++;
+//                 cuentaMulti++;
+//                 const arrMulti = [ingresarNumero, ingresarOp, ingresarNumeroSeg, "=", resultado];
+//                 arrMultiTotal.push(arrMulti.join(" "));
+//                  break;
+//             default:
+//                 alert("Ingresá una operación valida")
+//                 break;
+//         }
+//     } while (ingresarOp !== "+" && ingresarOp !== "-" && ingresarOp !== "/" && ingresarOp !== "*");
 
-}
+// }
 
-const mostrarTotal = () => {
-    alert(`El resultado es ${resultado}`)
-    entrada = confirm("¿Querés hacer otra cuenta?");
+// const mostrarTotal = () => {
+//     alert(`El resultado es ${resultado}`)
+//     entrada = confirm("¿Querés hacer otra cuenta?");
     
-    if (entrada === false) {
-    alert(`Hiciste en total ${cuenta} cuenta/s:
-    \n ${cuentaSuma} suma/s
-    \n ${cuentaResta} resta/s
-    \n ${cuentaDiv} division/es
-    \n ${cuentaMulti} multiplicacion/es`)
+//     if (entrada === false) {
+//     alert(`Hiciste en total ${cuenta} cuenta/s:
+//     \n ${cuentaSuma} suma/s
+//     \n ${cuentaResta} resta/s
+//     \n ${cuentaDiv} division/es
+//     \n ${cuentaMulti} multiplicacion/es`)
 
-    alert(`Estas fueron las cuentas que hiciste:
-    \n Sumas: ${arrSumaTotal.join(" | ")}
-    \n Restas: ${arrRestaTotal.join(" | ")}
-    \n Divisiones: ${arrDivTotal.join(" | ")}
-    \n Multiplicaciones: ${arrMultiTotal.join(" | ")}`);
-    } else {
-        init();
-    }  
+//     alert(`Estas fueron las cuentas que hiciste:
+//     \n Sumas: ${arrSumaTotal.join(" | ")}
+//     \n Restas: ${arrRestaTotal.join(" | ")}
+//     \n Divisiones: ${arrDivTotal.join(" | ")}
+//     \n Multiplicaciones: ${arrMultiTotal.join(" | ")}`);
+//     } else {
+//         init();
+//     }  
+// }
+
+// const init = () => {
+//     preguntaNumero();
+//     preguntaOperacion();
+//     mostrarTotal();
+// }
+// init();
+
+//###################################################################################
+//###################################################################################
+//###################################################################################
+
+//////////////////////// PRIMERA ENTREGA 21/02 ///////////////////////////////
+
+// variables globales
+let arrCompra = []; // array que uso que para guardar las compras
+let arrPrecioCompra = []; // array que uso para calcular el precio
+let arrNombreCompra = []; // array que uso al ultimo para al funcion borrar porque por alguna razono no lo puedo hacer con arrCompra
+let total; // variable que uso para mostrar el total
+
+class Videojuego{
+    constructor(id, nombre, precio){
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
 }
+// objetos
+const videojuegoUno = new Videojuego(1, "Cuphead", 3000);
+const videojuegoDos = new Videojuego(2, "Last of Us", 3500);
+const videojuegoTres = new Videojuego(3, "Little Nightmares", 2000);
+const videojuegoCuatro = new Videojuego(4, "Destiny 2", 2500);
+const videojuegoCinco = new Videojuego(5, "Red Dead Redemption 2", 4000);
+let arrVideojuegos = [videojuegoUno, videojuegoDos, videojuegoTres, videojuegoCuatro, videojuegoCinco]; //array de objetos
+const compra = () => {
+    let preguntaCompra;
+    let preguntaDeNuevo;
+    let precioCompra;
+    while (preguntaDeNuevo !== 2) {      
+        preguntaCompra = parseInt(
+            prompt(
+            "Que juego vas a elegir? Escribe el número del juego que quieras. \n1) Cuphead \n2) Last of Us \n3) Little Nightmares \n4) Destiny 2 \n5) Red Dead Redemption 2"
+            )); //pregunta para elija un numero
+            const someId = arrVideojuegos.some(el => el.id === preguntaCompra); // busca el numero de id en el array de objeto para saber si es true
 
-const init = () => {
-    preguntaNumero();
-    preguntaOperacion();
-    mostrarTotal();
+            if (someId === true) {
+                const finderId = arrVideojuegos.find(el => el.id === preguntaCompra); // busca la compra en el array
+                precioCompra = finderId.precio; // variable para tener guardado el precio de la compra individual
+                arrCompra.push(JSON.stringify(finderId)); // pushea a otro array las compras que vaya eligiendo
+                console.log(typeof arrCompra)
+                arrPrecioCompra.push(precioCompra); // pushea en un array el precio de todas las compras que vaya haciendo
+                arrNombreCompra.push(finderId); // pusheo el const que va encontrando lo que va eligiendo ocn el prompt
+                preguntaDeNuevo = parseInt(prompt("Desea llevar otro? \n\n1- Si \n2- No."));
+            } else {
+                alert("Por favor ingresá uno de los números");
+            }   
+    } 
 }
-init();
+const muestraPrecio = () => {
+    alert(`Estos son los juegos que compró:\n${arrCompra.join("\n")}`); // muestra los juegos elegidos. No se por que me los muestro ordenados uno abajo del otro y en la funcion borrar hago lo mismo y no sale asi
+    total = arrPrecioCompra.reduce((a, b) => a + b, 0); // usa esta funcion de orden superior para sumar los precios del array
+    alert(`El total sería $${total}`);    
+}
+const borrar = () => {
+    let preguntar = true;
+    let respuesta;
+    let borrarJuego;
+    let juegoEncontrado;
+    let precioJuegoBorrado;
+    while (preguntar) {        
+        respuesta = parseInt(prompt("Desea eliminar un juego? \n\n1- Sí \n2- No."));
+        if (respuesta == 1) {
+            borrarJuego = parseInt(prompt(`Introduzca el id del juego a eliminar:\n${JSON.stringify(arrNombreCompra)}`));
+            juegoEncontrado = arrNombreCompra.some(el => el.id === borrarJuego); // se fija es true o false la opcion elegida
+            const borrarJuegoId = arrNombreCompra.find(el => el.id === borrarJuego); // busca el juego borrado
+            const nombreJuegoBorrado = borrarJuegoId.nombre; // variable para guardar el nombre del juego borrado
+            precioJuegoBorrado = borrarJuegoId.precio; // variable para guardar el precio del juego borrado
+            if (juegoEncontrado == true) { 
+                    alert(`Eliminaste a:\n${nombreJuegoBorrado}`);
+                    // arrCompra = arrNombreCompra.filter(el => el.id !== borrarJuego); // pasa a un nuevo array todos los juegos menos el borrado
+                    arrNombreCompra = arrNombreCompra.filter(el => el.id !== borrarJuego); // pasa a un nuevo array todos los juegos menos el borrado
+                    alert(`Estos son los juegos que llevas ahora ${JSON.stringify(arrNombreCompra)}`);
+                    // alert(`Estos son los juegos que llevas ahora:\n${arrCompra.join("\n")}`);
+                    total = total - precioJuegoBorrado; // resto el precio del juego borrado al total de antes
+                    alert(`Ahora el total es de $${total}`);  
+                if (total == 0) {
+                    alert("Ya no hay juegos en el carrito")
+                    preguntar = false;
+                }
+            } else {
+                alert("No se encontro el juego que desea eliminar.");
+                respuesta = parseInt(prompt("Desea eliminar un juego \n\n1- Sí \n2- No."));
+            }
+        } else if (respuesta == 2) {
+            alert("Ha seleccionado no eliminar productos.");
+            alert(`Gracias por su compra. El total a abonar es $${total}`); 
+            preguntar = false;
+        } else {
+            alert("Ha seleccionado una opción no válida introduzca (1) si desea eliminar un juego o (2) en caso contrario.");
+            respuesta = parseInt(prompt("Desea eliminar un juego? \n\n1- Sí \n2- No."));
+        }
+    }
+}
+    compra();
+    muestraPrecio();
+    borrar();
 
 
 
 
 
 
+
+// class Videojuego{
+//     constructor(id, nombre, precio){
+//         this.id = id;
+//         this.nombre = nombre;
+//         this.precio = parseFloat(precio);
+//     }
+// }
+// const videojuegoUno = new Videojuego(1, "Cuphead", 3000);
+// const videojuegoDos = new Videojuego(2, "Last of Us", 3500);
+// const videojuegoTres = new Videojuego(3, "Little Nightmares", 2000);
+// const videojuegoCuatro = new Videojuego(4, "Destiny 2", 2500);
+// const videojuegoCinco = new Videojuego(5, "Red Dead Redemption 2", 4000);
+// const arrVideojuegos = [videojuegoUno, videojuegoDos, videojuegoTres, videojuegoCuatro, videojuegoCinco];
+
+
+// let mostrarProductos;
+
+
+// arrVideojuegos.forEach(recorrerArreglo);
+
+
+// function recorrerArreglo() {
+//     mostrarProductos += index + item; 
+// }
+// alert(JSON.stringify(arrVideojuegos));
